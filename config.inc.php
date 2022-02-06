60bc49d7ac3c36c1c01108dca5b9a8e06d0d066c
projekty.git@gmail.com <56201394+ajczi14@users.noreply.github.com>
2022-02-06 19:18:35 +0100

    Initial commit

---
+++ config.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = [
+    'db_host'     => 'localhost',
+    'db_name'     => 's401354',
+    'db_user'     => 's401354',
+    'db_password' => 'myhlj7jierjd'
+];
+    $config['recaptcha_public'] = '6LcrreYUAAAAAGeERM9Etdv8wlh81RCSK0LXuwfX';
+    $config['recaptcha_private'] = '6LcrreYUAAAAALTdoToeer_H4NZ1ECK4U76g0huL';

b3d2ff853b84987f56e3de4af09ca2ab22f1be67
projekty.git@gmail.com <56201394+ajczi14@users.noreply.github.com>
2022-02-08 21:52:28 +0100

    Add function to return sub in json

--- add.php
+++ add.php
@@ -14,11 +14,22 @@
     }
     //print_r($categoriesNames);
 
+   print_r($_POST['nameCat']);
+   /* if ($_POST['nameCat']) {
+        $query = "SELECT subcategoryName FROM Categories WHERE categoryName=".$_POST['nameCat'];
+        $result = $db=>query($query);
+        if ($result->num_rows >0) {
+            while($row = $result->fetch_assoc()) {
+                print_r($row);
+            }
+        }
+    } */
+
     if(empty($_POST))
     {
         echo $twig->render('add.html.twig',['session' => $_SESSION, 'data' => $date, 'categoriesNames' => $categoriesNames]);
     }
-    else {    
+    else {          
         try {
             if (empty($_FILES['image'])) {
                 throw new Exception('Image file is missing');

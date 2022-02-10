ef2c6585d152af65d92ff8485eeb6bf1b52260fa
mariannakubsik <maka.kubsik1@gmail.com>
2022-02-10 16:44:38 +0100

    messages & recaptcha

--- conversation.php
+++ conversation.php
@@ -1,26 +1,116 @@
 <?php
 
     include("connect.php");
+    $currentDate = date("Y-m-d H:i:s");
 
-    if(isset($_SESSION['id']) && isset($_GET['post']) && isset($_GET['person']) ) { 
-        
-        $idPost = $_GET['post'];
-        $idOtherUser = $_GET['person'];
+    if (isset($_SESSION['id'])) {
 
-        $messages = [];
-        $stmt = $dbh->prepare("SELECT um.message, u.login, um.createdTimeMessage, um.idUserSender
-            FROM UserPostMessage um INNER JOIN Users u ON um.idUserSender = u.idUser 
-                WHERE um.idPost = :idPost AND ((um.idUserSender = :idOtherUser AND um.idUserReceiver = :idMeUser) 
-                    OR (um.idUserSender = :idMeUser AND um.idUserReceiver = :idOtherUser)) ORDER BY createdTimeMessage");
-        $stmt->execute([':idPost' => $idPost, ':idOtherUser' => $idOtherUser, ':idMeUser' => $_SESSION['id']]);
+        if (isset($_GET['post']) && isset($_GET['person'])) {
+            $idPost = $_GET['post'];
+            $idOtherUser = $_GET['person'];
     
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $messages[] = $row;
-        }
+            $stmt_1 = $dbh->prepare("SELECT login FROM Users WHERE idUser = $idOtherUser");
+            $stmt_1->execute();
+            $otherUserLogin = $stmt_1->fetchColumn();
+    
+            $messages = [];
+            $stmt = $dbh->prepare("SELECT um.message, u.login, um.createdTimeMessage, um.idUserSender
+                FROM UserPostMessage um INNER JOIN Users u ON um.idUserSender = u.idUser 
+                    WHERE um.idPost = :idPost AND ((um.idUserSender = :idOtherUser AND um.idUserReceiver = :idMeUser) 
+                        OR (um.idUserSender = :idMeUser AND um.idUserReceiver = :idOtherUser)) ORDER BY createdTimeMessage");
+            $stmt->execute([':idPost' => $idPost, ':idOtherUser' => $idOtherUser, ':idMeUser' => $_SESSION['id']]);
+        
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $messages[] = $row;
+            }
+        } else {
+            if (isset($_GET['message']) && isset($_GET['post']) && isset($_POST['message']) ) {
 
-        echo $twig->render('conversation.html.twig', ['data' => $date, 'session' => $_SESSION, 'messages' => $messages]);
-    }
-    else {
+                $idPost =intval($_GET['post']);
+                $idReceiver = intval($_GET['message']);
+                $message = $_POST['message'];
+                
+                if (mb_strlen($message) >= 2 && mb_strlen($message) <= 200) {
+                    try {
+                        $stmt = $dbh->prepare ("INSERT INTO UserPostMessage (
+                        idUserPostMessage, message, idUserSender, idUserReceiver, idPost, createdTimeMessage) 
+                        VALUES (
+                            null, :message, :idUserSender, :idUserReceiver, :postID , '$currentDate') 
+                        ");
+        
+                    $stmt->execute([':message' => $message, ':idUserSender' => $_SESSION['id'], ':idUserReceiver' => $idReceiver, ':postID' => $idPost]);
+                    } 
+                    catch (PDOException $e) {
+                    }
+
+                    //header('Location: /conversation.php?post='$idPost'&person='$idReceiver'');
+                    //print_r("powrót");
+                    //exit();
+                }
+        
+                $stmt_1 = $dbh->prepare("SELECT login FROM Users WHERE idUser = $idReceiver");
+                $stmt_1->execute();
+                $otherUserLogin = $stmt_1->fetchColumn();
+        
+                $messages = [];
+                $stmt_2 = $dbh->prepare("SELECT um.message, u.login, um.createdTimeMessage, um.idUserSender
+                    FROM UserPostMessage um INNER JOIN Users u ON um.idUserSender = u.idUser 
+                        WHERE um.idPost = :idPost AND ((um.idUserSender = :idOtherUser AND um.idUserReceiver = :idMeUser) 
+                            OR (um.idUserSender = :idMeUser AND um.idUserReceiver = :idOtherUser)) ORDER BY createdTimeMessage");
+                $stmt_2->execute([':idPost' => $idPost, ':idOtherUser' => $idReceiver, ':idMeUser' => $_SESSION['id']]);
+            
+                while ($row = $stmt_2->fetch(PDO::FETCH_ASSOC)) {
+                    $messages[] = $row;
+                }
+
+                $idOtherUser = $idReceiver;
+
+            }
+        }
+        // elseif (isset($_GET['message']) && isset($_POST['message']) && isset($_GET['post'])) {
+
+        //     $idPost =intval($_GET['post']);
+        //     $idReceiver = intval($_GET['message']);
+        //     $message = $_POST['message'];
+                
+        //     // if (mb_strlen($message) >= 2 && mb_strlen($message) <= 200) {
+        //     //     try {
+        //     //         $stmt = $dbh->prepare ("INSERT INTO UserPostMessage (
+        //     //         idUserPostMessage, message, idUserSender, idUserReceiver, idPost, createdTimeMessage) 
+        //     //         VALUES (
+        //     //             null, :message, :idUserSender, :idUserReceiver, :postID , '$currentDate') 
+        //     //         ");
+        
+        //     //     $stmt->execute([':message' => $message, ':idUserSender' => $_SESSION['id'], ':idUserReceiver' => $idReceiver, ':postID' => $idPost]);
+        //     //     } 
+        //     //     catch (PDOException $e) {
+        //     //     }
+        //     //     header('Location: /conversation.php?post='$idPost'&person='$idReceiver'');
+        //     //     exit();
+        //     // }
+        //     // header('Location: /conversation.php?post='$idPost'&person='$idReceiver'');
+        //     // exit();
+
+        //     if (mb_strlen($message) >= 2 && mb_strlen($message) <= 200) {
+        //         try {
+        //             $stmt = $dbh->prepare ("INSERT INTO UserPostMessage (
+        //             idUserPostMessage, message, idUserSender, idUserReceiver, idPost, createdTimeMessage) 
+        //             VALUES (
+        //                 null, :message, :idUserSender, :idUserReceiver, :postID , '$currentDate') 
+        //             ");
+    
+        //         $stmt->execute([':message' => $message, ':idUserSender' => $_SESSION['id'], ':idUserReceiver' => $idReceiver, ':postID' => $idPost]);
+        //         } 
+        //         catch (PDOException $e) {
+        //         }
+        //         header('Location: /conversation.php?post='$idPost'&person='$idReceiver'');
+        //         print_r("powrót");
+        //         exit();
+        //     }
+        // }
+        echo $twig->render('conversation.html.twig', ['data' => $date, 'session' => $_SESSION, 'messages' => $messages, 'otherUserLogin' => $otherUserLogin, 'idPost' => $idPost, 'idOtherUser' => $idOtherUser]);
+
+    } else {
         header("Location:https://s401354.labagh.pl/main");
         exit();
     }
--- register.php
+++ register.php
@@ -20,15 +20,15 @@
             if (isset($_POST['g-recaptcha-response'])) {
                 $gRecaptchaResponse = $_POST['g-recaptcha-response'];
             }
-		    //$recaptcha = new \ReCaptcha\ReCaptcha($config['recaptcha_private']);
-		    //$resp = $recaptcha->verify($gRecaptchaResponse);
+		    $recaptcha = new \ReCaptcha\ReCaptcha($config['recaptcha_private']);
+		    $resp = $recaptcha->verify($gRecaptchaResponse);
 
                 $username = $_POST['login'];
                 $new_email= $_POST['email'];
                 $new_password_1= ($_POST['new_password_1']);
                 $new_password_2 = ($_POST['new_password_2']);
-//$resp->isSuccess() && 
-           if(preg_match('/^[a-zA-Z0-9\-\_\.]+\@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z]{2,5}$/D', $new_email) && preg_match('/^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\d.-]{0,100}$/D', $username) && $_POST['new_password_1'] === $_POST['new_password_2'])
+                
+           if($resp->isSuccess() && preg_match('/^[a-zA-Z0-9\-\_\.]+\@[a-zA-Z0-9\-\_\.]+\.[a-zA-Z]{2,5}$/D', $new_email) && preg_match('/^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\d.-]{0,100}$/D', $username) && $_POST['new_password_1'] === $_POST['new_password_2'])
            {
                 $hash_password_1 = password_hash($new_password_1, PASSWORD_DEFAULT);  
                 try {

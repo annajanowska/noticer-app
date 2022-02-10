b6c8d87df57cb7817c0d0823d6780721e392870b
mariannakubsik <maka.kubsik1@gmail.com>
2022-02-10 18:44:31 +0100

    chat fixed

--- message.php
+++ message.php
@@ -6,23 +6,37 @@
 
         $idOtherUser = 0;
         $conversations = [];
-        $stmt = $dbh->prepare("SELECT DISTINCT (idPost), idUserSender , idUserReceiver FROM UserPostMessage WHERE idUserSender = :idUserSender OR idUserReceiver = :idUserReceiver");
-        $stmt->execute([':idUserSender' => $_SESSION['id'], ':idUserReceiver' => $_SESSION['id']]);
+        $stmt = $dbh->prepare("SELECT DISTINCT idPost, IF(idUserSender = :idUser, idUserReceiver, idUserSender) as buyer, MAX(lastMsg) as lastMsg FROM (
+            SELECT idPost, idUserSender, idUserReceiver, MAX(createdTimeMessage) as lastMsg FROM UserPostMessage WHERE idUserSender = :idUser OR idUserReceiver = :idUser
+            GROUP BY idPost, idUserSender, idUserReceiver
+            ORDER BY lastMsg DESC
+        ) AS test
+        GROUP BY idPost, IF(idUserSender = :idUser, idUserReceiver, idUserSender);");
+
+        $stmt->execute([':idUser' => $_SESSION['id']]);
     
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
             $stmt_1 = $dbh->prepare("SELECT title FROM Posts WHERE idPost = :idPost");
             $stmt_1->execute([':idPost' => $row['idPost']]);
             $row['postTitle'] = $stmt_1->fetchColumn();
-            $stmt_1 = $dbh->prepare("SELECT login FROM Users WHERE idUser = :idUser");
-            if ($_SESSION['id'] == intval($row['idUserSender'])) {
-                $stmt_1->execute([':idUser' => $row['idUserReceiver']]);
-                $idOtherUser = $row['idUserReceiver'] ;
-            } else {
-                $stmt_1->execute([':idUser' => $row['idUserSender']]);
-                $idOtherUser = $row['idUserSender'];
-            }
-            $row['idOtherUser'] = $idOtherUser;
-            $row['otherUserName'] = $stmt_1->fetchColumn();
+
+            $stmt_2 = $dbh->prepare("SELECT login FROM Users WHERE idUser = :idUser");
+            $stmt_2->execute([':idUser' => $row['buyer']]);
+            $row['otherUserName'] = $stmt_2->fetchColumn();
+
+            // $stmt_1 = $dbh->prepare("SELECT login FROM Users WHERE idUser = :idUser");
+            // if ($_SESSION['id'] == intval($row['buyer'])) {
+            //     $stmt_1->execute([':idUser' => $row['idUserReceiver']]);
+            //     $idOtherUser = $row['idUserReceiver'] ;
+            // } else {
+            //     $stmt_1->execute([':idUser' => $row['buyer']]);
+            //     $idOtherUser = $row['idUserSender'];
+            // }
+
+            // $row['idOtherUser'] = $idOtherUser;
+            // $row['otherUserName'] = $stmt_1->fetchColumn();
+
             $conversations[] = $row;
         }
 
